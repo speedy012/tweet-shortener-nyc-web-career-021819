87954aa07d2106016ea922bf6c943f01bfe15928
Herman <hrivas5726@aol.com>
2019-02-10 14:33:53 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -37,7 +37,7 @@ def selective_tweet_shortener(tweet)
   tweet.split(" ").map do |phrase|
     if phrase.length > 140
       word_substituter(phrase)
-    elsif phrase.length <= 140
+    elsif phrase.length <= 130
       phrase
     end
   end
